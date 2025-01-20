eb8cf5845a65daa00e0b62be8032692742baf27b
taimoorhamza1999 <taimoorhamza1999@gmail.com>
2025-01-20 20:45:56 +0400

    sp

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    origin:'165.227.175.117',
+    port: 3000,
+  }
 })
