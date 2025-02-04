4575f76298277f3a6eecd5322db4eebb2cd12d95
Taimoor Hamza <taimoorhamza1999@gmail.com>
2025-02-04 11:10:44 +0400

    blogcard testing v2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -167,7 +167,7 @@ const Home = () => {
             </ScrollAnimation>
           </Suspense>
           <LoaderWrapper>
-            {showForm && <SignupForm />}
+            {/* {showForm && <SignupForm />} */}
 
             <Footer />
           </LoaderWrapper>
